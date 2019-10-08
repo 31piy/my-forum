013ae5b53cc73e68d59c946739386e4ef41e8ead
Piyush Sonagara <piyush.sonagara@cohesity.com>
2019-10-08 07:46:11 +0530

    chore: Setup DB seeds

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ApplicationRecord
+
+  belongs_to :parent, polymorphic: true
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+users = JSON.parse(File.read(File.expand_path('../users.json', __FILE__)))
+posts = JSON.parse(File.read(File.expand_path('../posts.json', __FILE__)))
+comments = JSON.parse(File.read(File.expand_path('../comments.json', __FILE__)))
+
+User.create(users)
+Post.create(posts)
+Comment.create(comments)
